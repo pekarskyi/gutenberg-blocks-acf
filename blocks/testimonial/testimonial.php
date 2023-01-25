e73bc95677dc1e68480eb2d8e9e7b789799cb8df
Mykola Pekarskyi <dizer7@gmail.com>
2023-01-25 17:54:34 +0200

    Master

--- blocks/testimonial/testimonial.php
+++ blocks/testimonial/testimonial.php
@@ -27,13 +27,9 @@
 
     <div class="col-testimonial-grid">
         <div class="testimonial-item">
-
-            <?php if($thumb == '') { ?>
-            <img src="<?php echo $main_path; ?>/blocks/testimonial/images/star.png"
-                alt="<?php echo esc_attr($testimonial_author); ?>" />
-            <?php } else { ?>
-            <img src="<?php echo esc_url($thumb); ?>" alt="<?php echo esc_attr($testimonial_author); ?>" />
-            <?php }  ?>
+		
+		<div class="testimonila-col">
+		<div class="left">
 
             <div class="testimonial-item-name"><?php echo esc_attr($testimonial_author); ?></div>
             <div class="testimonial-item-position"><?php echo esc_attr($testimonial_position); ?></div>
@@ -41,6 +37,20 @@
             <div class="testimonial-item-stars">
                 <?php wp_star_rating( ['rating'=>$testimonial_rating, 'type'=>'rating', 'number'=>0 ] );?>
             </div>
+			
+			</div>
+			
+			<div class="right">
+			
+			<?php if($thumb == '') { ?>
+            <img src="<?php echo $main_path; ?>/blocks/testimonial/images/star.png"
+                alt="<?php echo esc_attr($testimonial_author); ?>" />
+            <?php } else { ?>
+            <img src="<?php echo esc_url($thumb); ?>" alt="<?php echo esc_attr($testimonial_author); ?>" />
+            <?php }  ?>
+			
+			</div>
+			</div>
 
             <div class="testimonial-item-read-more" style="--line-clamp: 4">
                 <input id="testimonial-item-read-more-checkbox-<?php echo esc_html( $unique_id ); ?>" type="checkbox"
@@ -48,9 +58,18 @@
                 <p class="testimonial-item-read-more__text">
                     <?php echo esc_html( $testimonial_text ); ?>
                 </p>
+
+                <!-- Expand+Polylang -->
+                <?php if(ICL_LANGUAGE_CODE=='ru'): ?>
                 <label for="testimonial-item-read-more-checkbox-<?php echo esc_html( $unique_id ); ?>"
-                    class="testimonial-item-read-more__label" data-read-more="Показать все" data-read-less="Скрыть"
+                    class="testimonial-item-read-more__label" data-read-more="Развернуть" data-read-less="Свернуть"
                     aria-hidden="true"></label>
+                <?php elseif(ICL_LANGUAGE_CODE=='uk'): ?>
+                <label for="testimonial-item-read-more-checkbox-<?php echo esc_html( $unique_id ); ?>"
+                    class="testimonial-item-read-more__label" data-read-more="Розгорнути" data-read-less="Згорнути"
+                    aria-hidden="true"></label>
+                <?php endif; ?>
+
             </div>
         </div>
 
